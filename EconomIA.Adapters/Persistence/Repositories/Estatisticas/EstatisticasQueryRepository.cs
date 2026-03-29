ed3d9ad922307e4d0e8c26fd76c7378d9b5a6e7b
Lênio Corrêa <leniocorrea@gmail.com>
2026-03-29 19:05:22 -0300

    fix: otimizar query de estatisticas do dashboard usando reltuples

--- EconomIA.Adapters/Persistence/Repositories/Estatisticas/EstatisticasQueryRepository.cs
+++ EconomIA.Adapters/Persistence/Repositories/Estatisticas/EstatisticasQueryRepository.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using EconomIA.Domain.Repositories;
@@ -10,18 +9,29 @@ namespace EconomIA.Adapters.Persistence.Repositories.Estatisticas;
 public class EstatisticasQueryRepository(IDbContextFactory<EconomIAQueryDbContext> contextFactory) : IEstatisticas {
 	public async Task<EstatisticasGerais> ObterEstatisticasGeraisAsync(CancellationToken cancellationToken = default) {
 		await using var context = await contextFactory.CreateDbContextAsync(cancellationToken);
+		var conexao = context.Database.GetDbConnection();
+		await conexao.OpenAsync(cancellationToken);
 
-		var totalCompras = await context.Compras.LongCountAsync(cancellationToken);
-		var totalContratos = await context.Contratos.LongCountAsync(cancellationToken);
-		var totalAtas = await context.Atas.LongCountAsync(cancellationToken);
-		var totalItens = await context.ItensDaCompra.LongCountAsync(cancellationToken);
-		var totalOrgaosMonitorados = await context.OrgaosMonitorados.Where(x => x.Ativo).LongCountAsync(cancellationToken);
+		var sql = @"
+			SELECT
+				(SELECT reltuples::bigint FROM pg_class WHERE relname = 'compra') AS total_compras,
+				(SELECT reltuples::bigint FROM pg_class WHERE relname = 'contrato') AS total_contratos,
+				(SELECT reltuples::bigint FROM pg_class WHERE relname = 'ata') AS total_atas,
+				(SELECT reltuples::bigint FROM pg_class WHERE relname = 'item_da_compra') AS total_itens,
+				(SELECT COUNT(*) FROM orgao_monitorado WHERE ativo = true) AS total_orgaos_monitorados
+		";
+
+		await using var comando = conexao.CreateCommand();
+		comando.CommandText = sql;
+
+		await using var leitor = await comando.ExecuteReaderAsync(cancellationToken);
+		await leitor.ReadAsync(cancellationToken);
 
 		return new EstatisticasGerais(
-			TotalCompras: totalCompras,
-			TotalContratos: totalContratos,
-			TotalAtas: totalAtas,
-			TotalItens: totalItens,
-			TotalOrgaosMonitorados: totalOrgaosMonitorados);
+			TotalCompras: leitor.GetInt64(0),
+			TotalContratos: leitor.GetInt64(1),
+			TotalAtas: leitor.GetInt64(2),
+			TotalItens: leitor.GetInt64(3),
+			TotalOrgaosMonitorados: leitor.GetInt64(4));
 	}
 }
